bf8fc037b8a18eb2201481129aae1076ed67b5f7
DaiUnited <daiunited1505@gmail.com>
2024-11-01 13:08:39 +0700

    init Project + Models

---
+++ ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        public int Id { get; set; }
+        public string JobToDo { get; set; }
+        public string Status { get; set; } // Stages: "Incomplete", "In Progress", "Completed"
+        public DateTime StartDate { get; set; }
+    }
+}
