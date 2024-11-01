d144e8ef2e30c0b86a0d38b1a9a9c8f4736c32c6
DaiUnited <daiunited1505@gmail.com>
2024-11-01 13:34:45 +0700

    Entity Framework Core

--- ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
+++ ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf8fc037b8a18eb2201481129aae1076ed67b5f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
