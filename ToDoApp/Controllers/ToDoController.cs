93da0ebc2b1923e1e7d15b8c58c785358838111d
DaiUnited <daiunited1505@gmail.com>
2024-11-01 21:22:10 +0700

    init Controller + Create method

---
+++ ToDoApp/Controllers/ToDoController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoApp.Models;
+
+namespace ToDoApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly ToDoContext _context;
+
+        public ToDoController(ToDoContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var todos = _context.ToDoItems.ToList();
+            return Ok(todos);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] ToDoItem todo)
+        {
+            _context.ToDoItems.Add(todo);
+            _context.SaveChanges();
+            return Ok(todo);
+        }
+    }
+}
--- ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
+++ ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d144e8ef2e30c0b86a0d38b1a9a9c8f4736c32c6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad34daa715940f392b4f7553ff2745e2d4aafdc9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
