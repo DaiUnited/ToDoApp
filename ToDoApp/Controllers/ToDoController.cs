a665e2598ac8a31c5d24f964662e9dc9a704b088
DaiUnited <daiunited1505@gmail.com>
2024-11-01 23:02:04 +0700

    adding Update + Delete Method

--- ToDoApp/Controllers/ToDoController.cs
+++ ToDoApp/Controllers/ToDoController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using ToDoApp.Models;
+using System.Linq;
 
 namespace ToDoApp.Controllers
 {
@@ -21,12 +22,54 @@ namespace ToDoApp.Controllers
             return Ok(todos);
         }
 
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var todo = _context.ToDoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+            return Ok(todo);
+        }
+
         [HttpPost]
         public IActionResult Create([FromBody] ToDoItem todo)
         {
             _context.ToDoItems.Add(todo);
             _context.SaveChanges();
             return Ok(todo);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody] ToDoItem todo)
+        {
+            var existingToDo = _context.ToDoItems.Find(id);
+            if (existingToDo == null)
+            {
+                return NotFound();
+            }
+
+            existingToDo.JobToDo = todo.JobToDo;
+            existingToDo.Status = todo.Status;
+            existingToDo.StartDate = todo.StartDate;
+
+            _context.SaveChanges();
+            return Ok(existingToDo);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var todo = _context.ToDoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.ToDoItems.Remove(todo);
+            _context.SaveChanges();
+            return Ok();
         }
     }
 }
