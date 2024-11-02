8d2b76b1380229c18b90971aaea1f4edc9a331c3
DaiUnited <daiunited1505@gmail.com>
2024-11-02 08:43:57 +0700

    adding Bootstrap CSS + jQuery AJAX

--- ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
+++ ToDoApp/obj/Debug/net8.0/ToDoApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93da0ebc2b1923e1e7d15b8c58c785358838111d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a665e2598ac8a31c5d24f964662e9dc9a704b088")]
 [assembly: System.Reflection.AssemblyProductAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ToDoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
